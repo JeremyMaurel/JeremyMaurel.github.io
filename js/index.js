25c1c5997ec0f7eaf014acbdebfe5007d4d7c336
Jeremy Maurel <jeremy.maurel@oclock.school>
2024-03-22 15:27:40 +0100

    Publication

---
+++ js/index.js
@@ -0,0 +1,58 @@
+const button = document.querySelector("#button-menu");
+button.addEventListener("click", () => {
+  const menu = document.querySelector("#menu");
+  menu.classList.toggle("hidden");
+});
+
+let previousActiveNavItem;
+
+const downloadButton = document.querySelector("#download-button");
+downloadButton.addEventListener("click", downloadCV);
+
+document.addEventListener("DOMContentLoaded", setActiveLink);
+document.addEventListener("scroll", setActiveLink);
+
+document.querySelector(".contact__form").onsubmit = function () {
+  this.reset();
+};
+
+function downloadCV() {
+  const downloadLink = document.createElement("a");
+  downloadLink.href = "./docs/CV_Maurel_Jeremy.pdf";
+  downloadLink.download = "CV-Maurel_Jeremy";
+
+  // Ajouter le lien à la page et le cliquer
+  document.body.appendChild(downloadLink);
+  downloadLink.click();
+
+  document.body.removeChild(downloadLink);
+}
+function setActiveLink() {
+  const sections = document.querySelectorAll("section");
+
+  sections.forEach((section) => {
+    const id = section.id;
+    const navItem = document.querySelector(`.nav-list__item--${id}`);
+    const isVisible = isElementVisible(section);
+
+    if (isVisible) {
+      navItem.classList.add("current-section");
+
+      if (previousActiveNavItem && previousActiveNavItem !== navItem) {
+        previousActiveNavItem.classList.remove("current-section");
+      }
+      previousActiveNavItem = navItem;
+    } else {
+      navItem.classList.remove("current-section");
+    }
+  });
+
+  function isElementVisible(element) {
+    const rect = element.getBoundingClientRect();
+    const windowHeight = window.innerHeight;
+    const midHeight = (rect.top + rect.bottom) / 2;
+    const visible = midHeight >= 0 && midHeight < windowHeight;
+
+    return visible;
+  }
+}
